374e352b2f66f7f4b518d018895ebe3b9172f981
darn-it-dan <daniellee.tech@gmail.com>
2021-11-16 15:57:51 +0900

    extending class in hopes to fix deployment

--- src/main/java/org/danlee/cardealer/CardealerApplication.java
+++ src/main/java/org/danlee/cardealer/CardealerApplication.java
@@ -2,9 +2,10 @@ package org.danlee.cardealer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CardealerApplication {
+public class CardealerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardealerApplication.class, args);
